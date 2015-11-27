6b60ede0bee2a171db0ccb707bc4deba7fec7f47
lehmann-4178656ch <lehmann.4178656ch@gmail.com>
2015-11-27 10:39:16 +0100

    Add docs, version and other stuff

---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. django-issuetracker documentation master file, created by
+   sphinx-quickstart on Fri Nov 27 10:35:13 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to django-issuetracker's documentation!
+===============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- issuetracker/__init__.py
+++ issuetracker/__init__.py
@@ -1,2 +1,39 @@
-__VERSION__ = "0.0.1"
 default_app_config = 'issuetracker.apps.IssuetrackerAppConfig'
+VERSION = ('16', '04', '0', 'dev', '0')
+
+
+def get_version():
+    """ Builds readable version from VERSION tuple.
+    If the flag part evaluates to true it is added to the version.
+    """
+    if VERSION[3]:
+        return '{year}.{month}.{flag}'.format(
+            year=VERSION[0],
+            month=VERSION[1],
+            flag=VERSION[3]
+        )
+    return '{year}.{month}'.format(
+        year=VERSION[0],
+        month=VERSION[1]
+    )
+
+
+def get_release():
+    """ Builds readable release from VERSION tuple.
+    If the flag part evaluates to true it is added to the version with the
+    suffix.
+    """
+    if VERSION[3]:
+        return '{year}.{month}.{patch}.{flag}{suffix}'.format(
+            year=VERSION[0],
+            month=VERSION[1],
+            patch=VERSION[2],
+            flag=VERSION[3],
+            suffix=VERSION[4],
+        )
+    return '{year}.{month}.{patch}'.format(
+        year=VERSION[0],
+        month=VERSION[1],
+        patch=VERSION[2]
+    )
+
